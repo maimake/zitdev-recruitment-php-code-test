e70ae698bf6253735f7a4ceda1957e0dbd51c615
麦钟文 <maizhongwen@kuaizi.ai>
2022-07-01 15:54:13 +0800

    题目 2

--- tests/App/DemoTest.php
+++ tests/App/DemoTest.php
@@ -9,6 +9,9 @@
 
 namespace Test\App;
 
+use App\App\Demo;
+use App\Service\AppLogger;
+use App\Util\HttpRequest;
 use PHPUnit\Framework\TestCase;
 
 
@@ -20,5 +23,16 @@ class DemoTest extends TestCase {
 
     public function test_get_user_info() {
 
+        $response = '{ "error": 0, "data": { "id": 1, "username": "hello world" } }';
+        $stub_request = $this->createStub(HttpRequest::class);
+        $stub_request->method('get')->willReturn($response);
+        
+        $expect = json_decode($response, true)['data'];
+        
+        $service = new Demo(new AppLogger(), $stub_request);
+        $res = $service->get_user_info();
+        
+        $this->assertEquals($expect, $res);
+        
     }
 }
